0d14663ee9ecf5fbe221c57cdd44e7c101c5da16
Soumyajit <sensoumyajit73@gmail.com>
2022-12-11 12:39:27 +0530

    Master Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:new_app/home_page.dart';
 void main(){
   runApp(MyApp());
 }
@@ -7,14 +8,8 @@ class MyApp extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home:Material(
-        child: Center(
-          child: Container(
-            child: const Text("Welcome to flutter"),
-          ),
-        ),
-      ) ,
+    return const MaterialApp(
+      home:HomePage() ,
     );
   }
 }
